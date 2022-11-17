f9c110d9a81d9b99c110db652a39a8f6a4cf9753
JasonDai1219 <114436475+JasonDai1219@users.noreply.github.com>
2022-11-17 14:07:21 -0800

    AAA
    aaa

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,21 @@
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.*;
 
 public class TestListExamples {
   // Write your grading tests here!
+  @Test
+  public void testOrder() {
+    ArrayList<String> expected = new ArrayList<>();
+    expected.add("a");
+    expected.add("b");
+    expected.add("c");
+
+    List<String> result = ListExamples.filter(expected, sc);
+
+    assertEquals(expected, result);
+  }
 }
--- grade.sh
+++ grade.sh
@@ -5,3 +5,14 @@ set -e
 rm -rf student-submission
 git clone $1 student-submission
 
+cd student-submission
+if [-e ListExamples.java]
+then 
+    echo "The submission has wrong name"
+fi
+cp TestListExamples student-submission
+
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java 2> compile-err.txt
+
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
+
