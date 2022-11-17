94c384a73780daca7d0d96849d20fb58d91fa1d8
JasonDai1219 <114436475+JasonDai1219@users.noreply.github.com>
2022-11-17 14:55:07 -0800

    Changed

--- TestListExamples.java
+++ TestListExamples.java
@@ -5,17 +5,38 @@ import java.util.List;
 
 import org.junit.*;
 
+class VowelChecker implements StringChecker {
+  public boolean checkString(String s) {
+    ArrayList<String> storing = new ArrayList<>();
+    storing.add("a");
+    storing.add("A");
+    storing.add("e");
+    storing.add("E");
+    storing.add("i");
+    storing.add("I");
+    storing.add("o");
+    storing.add("O");
+    storing.add("u");
+    storing.add("U");
+    if(storing.contains(s)) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+}
 public class TestListExamples {
   // Write your grading tests here!
   @Test
-  public void testOrder() {
-    ArrayList<String> expected = new ArrayList<>();
-    expected.add("a");
-    expected.add("b");
-    expected.add("c");
-
-    List<String> result = ListExamples.filter(expected, sc);
-
+  public void testFilter() {
+    ArrayList<String> testing = new ArrayList<>();
+    testing.add("a");
+    testing.add("b");
+    testing.add("c");
+    VowelChecker sc = new VowelChecker();
+    List<String> result = ListExamples.filter(testing, sc);
+    List<String> expected = new ArrayList<>();
     assertEquals(expected, result);
   }
 }
--- grade.sh
+++ grade.sh
@@ -1,18 +1,25 @@
 # Create your grading script here
 
-set -e
 
 rm -rf student-submission
 git clone $1 student-submission
 
 cd student-submission
-if [-e ListExamples.java]
+if [ -e "ListExamples.java" ]
 then 
+    echo "true"
+else 
     echo "The submission has wrong name"
+    exit
 fi
-cp TestListExamples student-submission
+cp ../TestListExamples.java ./
 
-javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java 2> compile-err.txt
-
-java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
-
+javac ListExamples.java
+javac TestListExamples.java
+if [[ $? == 0 ]]
+then 
+    javac -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar *.java
+    java -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
+else
+    echo "Files are not compiling"
+fi
