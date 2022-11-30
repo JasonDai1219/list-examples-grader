8083bd455433c0d83c5cf1b1f4d83ce9d96f36da
JasonDai1219 <114436475+JasonDai1219@users.noreply.github.com>
2022-11-30 14:11:05 -0800

    Update TestListExamples.java

--- TestListExamples.java
+++ TestListExamples.java
@@ -5,42 +5,21 @@ import java.util.List;
 
 class IsMoon implements StringChecker {
   public boolean checkString(String s) {
-    return s.equalsIgnoreCase("moon");
+    return s.contains("A") || s.contains("a");
   }
 }
 
 public class TestListExamples {
-  @Test(timeout = 500)
-  public void testMergeRightEnd() {
-    List<String> left = Arrays.asList("a", "b", "c");
-    List<String> right = Arrays.asList("a", "d");
-    List<String> merged = ListExamples.merge(left, right);
+  @Test
+  public void testAoracontainer() {
+    List<String> test = Arrays.asList("a", "AAAAA", "");
+    List<String result = new ArrayList<>();
+    for (String i: test) {
+      if(this.checkString(i) == true) {
+          result.add(i);
+      }
+    }
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
-    assertEquals(expected, merged);
-  }
-
-  @Test(timeout = 500)
-  public void testMergeLeftEnd() {
-    List<String> left = Arrays.asList("a", "b", "z");
-    List<String> right = Arrays.asList("a", "d");
-    List<String> merged = ListExamples.merge(left, right);
-    List<String> expected = Arrays.asList("a", "a", "b", "d", "z");
-    assertEquals(expected, merged);
-  }
-
-  @Test(timeout = 500)
-  public void testFilterSingle() {
-    List<String> input = Arrays.asList("Moon", "MOO", "moo");
-    List<String> expect = Arrays.asList("Moon");
-    List<String> filtered = ListExamples.filter(input, new IsMoon());
-    assertEquals(expect, filtered);
-  }
-
-  @Test(timeout = 500)
-  public void testFilterMulti() {
-    List<String> input = Arrays.asList("Moon", "MOO", "moon", "MOON");
-    List<String> expect = Arrays.asList("Moon", "moon", "MOON");
-    List<String> filtered = ListExamples.filter(input, new IsMoon());
-    assertEquals(expect, filtered);
+    assertEquals(expected, result);
   }
 }
